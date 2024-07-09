222755c00211400c6a77445d0b645d5cc581aed5
Kássio Silva <kassio.slva@gmail.com>
2024-07-09 16:29:56 -0300

    remove: remove imports and unused code

--- app/src/main/java/com/example/todolistcompose/ui/components/Header.kt
+++ app/src/main/java/com/example/todolistcompose/ui/components/Header.kt
@@ -6,13 +6,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,35 +27,6 @@ fun Header(modifier: Modifier = Modifier) {
       contentDescription = null,
       modifier = Modifier.align(Alignment.Center)
     )
-
-//    Row(
-//      horizontalArrangement = Arrangement.spacedBy(4.dp),
-//      modifier = Modifier
-//        .fillMaxWidth()
-//        .align(Alignment.BottomCenter)
-//        .padding(horizontal = 24.dp)
-//        .offset(y = 24.dp)
-//    ) {
-//      InputAddTask(
-//        value = taskValue,
-//        onValueChange = { handleTaskValue(it) },
-//        modifier = Modifier.weight(1f),
-//        onKeyboardActionDone = { handleSubmit() }
-//      )
-//
-//      IconButton(
-//        onClick = { handleSubmit() },
-//        modifier = Modifier
-//          .background(color = BlueDark, shape = RoundedCornerShape(6.dp))
-//          .size(54.dp)
-//      ) {
-//        Icon(
-//          painter = painterResource(R.drawable.plus),
-//          contentDescription = "Add",
-//          tint = Gray100,
-//        )
-//      }
-//    }
   }
 }
 
